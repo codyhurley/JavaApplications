6d9c01559e80962f080e9ab259adc145247b382c
codyhurley <codyrhurley@gmail.com>
2017-08-23 20:24:16 -0500

    Cody Hurley's Time.java assignment
    Time.java

---
+++ Time.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Time {
+
+	public static void main(String[] args) {
+		
+		Date d = new Date();
+		
+		System.out.println("At the current execution of this program, the local time is " + d + ".");
+		
+		System.out.println(" ");
+		
+		
+		
+	}
+
+}
