bff94320c647ae23646c35ce9cef21139db92e6a
aktunc-STMU <aktunc@gmail.com>
2017-08-30 00:09:00 -0500

    Check comments

--- Time.java
+++ Time.java
@@ -10,7 +10,9 @@ public class Time {
 		
 		System.out.println(" ");
 		
-		
+		//the program gets the date but does not return the hours, minutes, seconds passed since Unix epoch
+		//Get the milliseconds passed from the Unix epoch using System.currentTimeMillis() and assign it to a long variable.
+		//Then convert the value to hours, minutes, seconds by dividing it by 1000 to seconds then by 60 to minutes etc.
 		
 	}
 
