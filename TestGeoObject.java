96d216eca5a38e7521c379672e5cbd54217c4ad5
codyhurley <codyrhurley@gmail.com>
2017-09-19 13:25:56 -0500

    Add files via upload

---
+++ TestGeoObject.java
@@ -0,0 +1,14 @@
+
+public class TestGeoObject implements Comparable{
+
+	public static void main(String[] args) {
+		
+		Rectangle r = new Rectangle (5, 7);
+		Circle c = new Circle (6);
+		
+		
+		
+		
+	}
+
+}
