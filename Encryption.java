39a13a72b0c227b251e06196ddfcfd930581d9af
codyhurley <codyrhurley@gmail.com>
2017-10-11 21:48:35 -0500

    Encryption and Decryption Challenge
    I'm not quite finished, but I'm uploading these so that I at least have SOMETHING turned in.

---
+++ Encryption.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+public class Encryption {
+
+	public static void main(String[] args) throws IOException{
+		
+		int reader;
+		boolean checkLoop = false;
+		
+		try {
+			
+			FileInputStream input = new FileInputStream ("BinarySum.dat");
+			DataInputStream inputFile = new DataInputStream(input);
+			
+			FileOutputStream output = new FileOutputStream("BinarySum.dat");
+			DataOutputStream outputFile = new DataOutputStream(output);
+			
+			do {
+				
+				reader = inputFile.readChar() + 10;
+				outputFile.writeChar(reader);
+				System.out.println(reader);
+				
+			} while (!checkLoop);
+			
+			inputFile.close();
+			output.close();
+			
+		} catch (EOFException e) {
+			
+			checkLoop = true;
+			
+		} finally {
+			
+			
+			
+		}
+		
+	}
+
+}
